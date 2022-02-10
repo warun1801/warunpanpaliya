29696044b671d49543273e1177e32ee987c9b1e3
Warun Panpaliya <warun1801@gmail.com>
2022-02-10 17:03:40 +0530

    add

--- next.config.js
+++ next.config.js
@@ -1,13 +1,2 @@
 const withImamges = require('next-images');
 module.exports = withImamges();
-module.exports = {
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"]
-        });
-        return config;
-    },
-    basePath: "",
-    assetPrefix: ''
-}
